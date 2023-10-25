90368fe7bd11f9528ff6c9bb80db72438987f44f
AnasHany1 <63459431+AnasHany1@users.noreply.github.com>
2023-10-25 23:37:49 +0300

    added check prime functions

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-def calculate_sum(*args):
-    return sum(args)
-
+def check_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
