7b93e0461e0f4b590235345b7b6e802aaa7e7168
AnasHany1 <63459431+AnasHany1@users.noreply.github.com>
2023-10-25 23:45:48 +0300

    Added Count Vowels Functions

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-def calculate_average(*args):
-    return sum(args) / len(args)
+def count_vowels(input_string):
+    vowels = "AEIOUaeiou"
+    return sum(input_string.count(vowel) for vowel in vowels)
