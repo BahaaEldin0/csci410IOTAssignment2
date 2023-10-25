0f2be60255b926812bc80d3731f09dc344ed8035
Atrozy <abdelrahmanatrozy400@gmail.com>
2023-10-25 23:51:21 +0300

    prime_factorization

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-def calculate_sum(*args):
-    return sum(args)
+def prime_factorization(number):
+    factors = []
+    divisor = 2
+    while number > 1:
+        if number % divisor == 0:
+            factors.append(divisor)
+            number //= divisor
+        else:
+            divisor += 1
+    return factors
+    
 
