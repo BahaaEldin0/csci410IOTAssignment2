54901e9959d4c064d6e320e4aaa45353ed5da23a
AnasHany1 <63459431+AnasHany1@users.noreply.github.com>
2023-10-25 23:39:17 +0300

    added Calculate Average Function

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-def check_prime(num):
-    if num <= 1:
-        return False
-    if num <= 3:
-        return True
-    if num % 2 == 0 or num % 3 == 0:
-        return False
-    i = 5
-    while i * i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+def calculate_average(*args):
+    return sum(args) / len(args)
