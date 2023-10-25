1cee56413b566d11ed347609dfff7ac96383ceb3
Atrozy <abdelrahmanatrozy400@gmail.com>
2023-10-25 23:55:53 +0300

    gcd

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def prime_factorization(number):
-    factors = []
-    divisor = 2
-    while number > 1:
-        if number % divisor == 0:
-            factors.append(divisor)
-            number //= divisor
-        else:
-            divisor += 1
-    return factors
-    
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
